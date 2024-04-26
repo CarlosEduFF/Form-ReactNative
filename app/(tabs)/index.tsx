5e193381988d1ead5ca04c86c561b9f6147d093f
CarlosEduFF <Carlos.farias11@etec.sp.gov.br>
2024-04-25 23:47:30 -0300

    Criação de exemplo de formulário em React Native

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,111 @@
-import { StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, View, Button, TextInput, Alert, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+export default function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [password1, setPassword1] = useState('');
+  const [email, setEmail] = useState('');
+  const [dataN, setDatan] = useState('');
 
-export default function TabOneScreen() {
+  const handleLogin = () => {
+    Alert.alert('Credenciais', `Nome: ${username}\nEmail: ${email}\nData de Nascimento: ${dataN}\nSenha: ${password} `);
+  };
+  const limparDados = () => {
+    setUsername('');
+    setPassword('');
+    setPassword1('');
+    setEmail('');
+    setDatan('');
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <ScrollView style={styles.container}>
+      <Text style={styles.title}>Criar Conta</Text>
+
+      <Text style={styles.label}>Digite nome de usuário:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setUsername}
+        value={username}
+        placeholder="Digite seu nome de usuário"
+      />
+      <Text style={styles.label}>Digite email do usuário:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setEmail}
+        value={email}
+        placeholder="Digite seu email de usuário"
+      />
+      <Text style={styles.label}>Digite a data de nascimento de usuário:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setDatan}
+        value={dataN}
+        placeholder="Digite sua data de nascimento"
+        
+      />
+      <Text style={styles.label}>Digite a senha de usuário:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setPassword}
+        value={password}
+        placeholder="Digite sua senha"
+        secureTextEntry
+      />
+      <Text style={styles.label}>Confirme a senha de usuário:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setPassword1}
+        value={password1}
+        placeholder="Confirme sua senha"
+        secureTextEntry
+      />
+      
+      <Button
+        title="Entrar"
+        onPress={handleLogin}
+      />
+      <Text></Text>
+      
+      <TouchableOpacity
+        style={styles.button}
+        onPress={limparDados}
+      >
+        <Text style={styles.buttonText}>Apagar</Text>
+      </TouchableOpacity>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: 'white',
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    textAlign: 'center',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 15,
+    paddingLeft: 10,
+  },
+  button: {
+    backgroundColor: '#ff0000', // Isso faz o botão ser vermelho
+    padding: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white', // Isso faz o texto do botão ser branco
   },
 });
